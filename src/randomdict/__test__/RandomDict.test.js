8b7aa2293254a4eefe053c975928acfd8347f233
Vjacheslav Borisov <slavb@ilb.ru>
2020-04-02 12:43:37 +0400

    fix tests

--- src/randomdict/__test__/RandomDict.test.js
+++ src/randomdict/__test__/RandomDict.test.js
@@ -1,10 +1,11 @@
 const RandomDict = require('../RandomDict');
 const RandomDictConst = require('../RandomDictConst');
+//configure for test reproductivity 
+const seedrandom = require('seedrandom');
+seedrandom('hello.', { global: true });
 
 const rd = new RandomDict(RandomDictConst.DICT_RU_NAMES_MALE);
+
 test('not empty string', () => {
-    expect(!!rd.getValue()).toBe(true);
-});
-test('not same string', () => {
-    expect(rd.getValue() != rd.getValue()).toBe(true);
+    expect(rd.getValue()).toBe('Максим');
 });
