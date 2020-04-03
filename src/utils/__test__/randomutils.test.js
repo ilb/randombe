62df392e539cc3b23a36164048e39778ba413009
Vjacheslav Borisov <slavb@ilb.ru>
2020-04-03 09:10:20 +0400

    randomLength method

--- src/utils/__test__/randomutils.test.js
+++ src/utils/__test__/randomutils.test.js
@@ -4,6 +4,8 @@ const seedrandom = require('seedrandom');
 seedrandom('hello.', { global: true });
 
 test('innfl', () => {
-    expect(randomutils.randomFloat(1,100)).toBe(63.452628248115);
-    expect(randomutils.randomInt(1,100)).toBe(93);
+    expect(randomutils.randomFloat(1,100)).toBe(5.074763933686807);
+    expect(randomutils.randomInt(1,100)).toBe(62);
+    expect(randomutils.randomLength(1)).toBe(3);
+    expect(randomutils.randomLength(5)).toBe(35621);
 });
--- src/utils/randomutils.js
+++ src/utils/randomutils.js
@@ -1,12 +1,33 @@
+/**
+ * random float value 
+ * @param {*} min minimum value 
+ * @param {*} max  maximum value
+ */
 function randomFloat(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+  return Math.random() * (max - min) + min;
+}
 
-  function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  
-  module.exports = {
-    randomFloat,
-    randomInt
-  }
+/**
+ * random int value
+ * @param {*} min minimum value 
+ * @param {*} max maximum value
+ */
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/**
+ * random value of specified length 
+ * @param {*} length 
+ */
+function randomLength(length) {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return randomInt(min, max);
+}
+
+module.exports = {
+  randomFloat,
+  randomInt,
+  randomLength
+}
