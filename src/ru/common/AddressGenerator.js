c1cfd60fd0c28884f06a5bdec8507a77f00ed2bb
Vjacheslav Borisov <slavb@ilb.ru>
2021-10-29 10:20:53 +0400

    addess generator stub

---
+++ src/ru/common/AddressGenerator.js
@@ -0,0 +1,12 @@
+const randomutils = require('../../utils/randomutils');
+
+class AddressGenerator {
+
+    constructor() {
+    }
+    getValue() {
+        return "643,РОССИЯ,426073,18,УДМУРТСКАЯ РЕСП,,ИЖЕВСК Г,,ПУШКИНСКАЯ УЛ,268,,,";
+    }
+}
+
+module.exports = AddressGenerator;
